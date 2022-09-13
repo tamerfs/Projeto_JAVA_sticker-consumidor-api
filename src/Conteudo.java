02f76cbf53a2f18f77885ff93b17782908d7ea9f
tamerfs <tamer.srhn@gmail.com>
2022-09-13 15:52:11 -0300

    Initial commit

---
+++ src/Conteudo.java
@@ -0,0 +1,20 @@
+public class Conteudo {
+    
+    private final String titulo;
+    private final String urlimagem;
+
+    
+
+    public Conteudo(String titulo, String urlimagem) {
+        this.titulo = titulo;
+        this.urlimagem = urlimagem;
+    }
+    public String getTitulo() {
+        return titulo;
+    }
+    public String getUrlimagem() {
+        return urlimagem;
+    }
+
+    
+}
